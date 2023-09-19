37d16b189dc88273f928ce39f0a818bcdfed1ec7
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-19 20:05:16 +0200

    Solution fix

--- Toetsoefeningen/04 bliksem/solution/solution.nl.py
+++ Toetsoefeningen/04 bliksem/solution/solution.nl.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 tijd = int( input( 'Geef het aantal seconden in:' ) )
 
 afstand = round( ( tijd * 343 ) / 1000, 3 )
 
-print()
 print( 'De blikseminslag was', afstand, 'km van je verwijderd.' )
