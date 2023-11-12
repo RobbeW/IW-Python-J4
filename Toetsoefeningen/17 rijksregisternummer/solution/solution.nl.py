fad62a247e35850682ec364b5ee7aff5cb13b5e4
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-11-12 15:41:26 +0100

    Fix solution

--- Toetsoefeningen/17 rijksregisternummer/solution/solution.nl.py
+++ Toetsoefeningen/17 rijksregisternummer/solution/solution.nl.py
@@ -12,5 +12,4 @@ if jaar >= 2000:
 cg = 97 - som % 97
 
 # Weergave op het scherm
-print()
 print( "Het rijksregisternummer is "+str(jaar%100).zfill(2)+"."+str(maand).zfill(2)+"."+str(dag).zfill(2)+"-"+str(dagteller).zfill(3)+"-"+str(cg).zfill(2)+"." )
