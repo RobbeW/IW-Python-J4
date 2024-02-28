df826760eae4d72d55fa9c350feefb75d8281bc3
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-28 23:25:24 +0100

    Extra pattern exercise

---
+++ Toetsoefeningen/29 Deelbaar door 7/solution/solution.nl.py
@@ -0,0 +1,28 @@
+# Invoer
+getal = int(input("Geef het getal in: "))
+
+# Berekening
+som_even = 0
+som_oneven = 0
+i = 0
+
+voorwaarde = True
+while voorwaarde:
+    i += 1
+    cijfer = getal % 10
+    getal = getal // 10
+    if i % 2 == 0:
+        som_even += cijfer
+    else:
+        som_oneven += cijfer
+    
+    if getal == 0:
+        voorwaarde = False
+
+# Weergave
+print("De som van de getallen op de oneven rang is:", som_oneven)
+print("De som van de getallen op de even rang is:", som_even)
+if (som_even - som_oneven) % 11 == 0:
+    print("Dit getal is deelbaar door 11!")
+else:
+    print("Dit getal is NIET deelbaar door 11!")
