d301aa9b490f79c648c480df677b5df0f6e5b218
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-05-02 00:47:08 +0200

    Fixing solution

--- Toetsoefeningen/32 Bijna gelijk/solution/solution.nl.py
+++ Toetsoefeningen/32 Bijna gelijk/solution/solution.nl.py
@@ -5,7 +5,6 @@ x = float(input( "Geef een eerste getal in: ") )
 y = float(input( "Geef een eerste getal in: ") )
 delta = float(input( "Geef een tolerantie in: ") )
 
-print()
 if bijnagelijk(x, y, delta):
     print( x, "en", y, "zijn bijna aan elkaar gelijk.")
 else:
