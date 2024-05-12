8bf59837a5cbb923a6217ff3675ed0863525b254
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-05-12 21:44:01 +0200

    Initial tuples exercise

---
+++ 03 Geavanceerde datatypes/05 Tuples/description/description.nl.md
@@ -0,0 +1,35 @@
+## Tupels
+
+Een tupel is ook een **geordend** datatype dat meerdere elementen kan bevatten van een ander datatype. Dit datatype wordt gedefinieerd met behulp van `( )` en kommas tussen de elementen. Via `len()` bepaal je de lengte van de tupel. 
+
+```python
+tupel = ( -5, 6, 8 )
+print( tupel )         # Dit print ["banaan", "appel", "mango"]
+print( len( tupel ) )  # Dit print 3, want de tupel bevat 3 elementen
+```
+
+Tupels zijn zeer gelijkaardig als lijsten, ze worden vooral gebruikt om met coördinaten te werken.
+
+```python
+A = ( 1, 3)
+print( 'Punt A heeft x-coördinaat', A[0], 'en y-coördinaat', A[1] )
+```
+
+Het belangrijkste **verschil** met lijsten is dat je ze *niet meer kan aanpassen* eenmaal aangemaakt.
+
+
+## Opgave
+De (Euclidische) afstand $$\mathsf{d}$$ van de oorsprong tot een punt $$mathsf{P(x,y)}$$ wordt gegeven door de volgende formule:
+
+$$
+    \mathsf{d = \sqrt{x^2+y^2}}
+$$
+
+Vul onderstaande functie `afstand( coordinaat )` aan die de afstand van een bepaalde coördinaat tot de oorsprong berekent. Rond hierbij af op 2 cijfers.
+
+#### Voorbeeld
+
+```
+>>> afstand( (3.0,4.0) )
+5.0
+```
---
+++ 03 Geavanceerde datatypes/05 Tuples/preparation/generator.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import importlib
+import random
+import ruamel.yaml
+import subprocess
+
+yaml = ruamel.yaml.YAML()
+
+# set fixed seed for generating test cases
+random.seed(12345678)
+
+# locate evaldir
+evaldir = os.path.join('..', 'evaluation')
+if not os.path.exists(evaldir):
+    os.makedirs(evaldir)
+
+# locate solutiondir
+solutiondir = os.path.join('..', 'solution')
+if not os.path.exists(solutiondir):
+    os.makedirs(solutiondir)
+
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+
+
+module_name = 'solution'
+file_path = os.path.join(solutiondir, 'solution.nl.py')
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+# generate test data
+ntests= 20
+cases = [ (3.0,4.0), (4.0,3.0),(0.0,0.0)]
+while len(cases) < ntests:
+    point = tuple( round(random.uniform(-10,10),1) for _ in range(2))
+    cases.append(point)
+    
+# generate unit tests for functions
+yamldata = []
+
+# input, expression, statement or stdin?
+input = 'stdin'
+# output, stdout or return?
+output = 'stdout'
+tabtitle = "Feedback"
+
+yamldata.append( {'tab': tabtitle, 'contexts': []})
+
+for i in range(len(cases)):
+    test = cases[i]
+    yamldata[0]['contexts'].append( {'testcases' : []})
+        
+    # generate test expression
+    #
+    expression_name = f"afstand( {test} )"
+    result = module.afstand( test )
+
+    print(result)
+    # setup for return expressions
+    testcase = { "expression": expression_name, "return": result }
+    yamldata[0]['contexts'][i]["testcases"].append( testcase)
+
+write_yaml(yamldata)
