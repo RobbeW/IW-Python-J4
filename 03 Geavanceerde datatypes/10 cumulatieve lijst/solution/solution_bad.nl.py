96669ae42abb401e22f8f508208efb7542639dc2
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2024-05-26 23:34:47 +0200

    Fix bad solution

--- 03 Geavanceerde datatypes/10 cumulatieve lijst/solution/solution_bad.nl.py
+++ 03 Geavanceerde datatypes/10 cumulatieve lijst/solution/solution_bad.nl.py
@@ -3,11 +3,9 @@ def somlijst( lijst ):
     nieuw = []
     for i in range(n):
         som = 0
-        for j in range(i):
+        for j in range(i+1):
             som += lijst[j]
         
         nieuw.append( som )
     
     return nieuw
-
-print( somlijst( [1, 5, 6, -2, 0, 5, 9] ))
