5bb6b9e47c336133f336794f8efcff4b0da2662c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-20 23:38:17 +0200

    Fix to TESTed

--- 01 Herhaling Python J3/Evaluatie/25 Archimedes/solution/solution.nl.py
+++ 01 Herhaling Python J3/Evaluatie/25 Archimedes/solution/solution.nl.py
@@ -4,7 +4,7 @@ n = int(input("Geef het aantal termen op: "))
 # Berekening
 som = 0
 for i in range(n):
-    som += 1/4**(i+1)
+    som += 1 / 4**(i + 1)
 
 # Weergave
 print( "Bij", n, "termen benadert men de Archimedes reeks als", round(som, 9 ) )
