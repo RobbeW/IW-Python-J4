2cea0acf7f0fb578ae94f19c616ebcb6f859c7e2
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-20 10:17:46 +0200

    extra solution

--- 01 Herhaling Python J3/07e Bosbeheer/solution/solution_break.nl.py
+++ 01 Herhaling Python J3/07e Bosbeheer/solution/solution_break.nl.py
@@ -8,13 +8,14 @@ bijplanten = int(input("Geef het aantal nieuwe bomen in: "))
 # Berekening
 print("Er zijn eerst", n, "bomen in het bos.")
 i = 0
-vorige = n - 1
-while vorige != n:
-    vorige = n
+while True:
     i += 1
     omhakken = math.floor( n*percentage/100 )
-    n = n - omhakken + bijplanten
-    print("na", i, "jaar zijn er", n, "bomen.")
-    
+    nieuw = n - omhakken + bijplanten
+    print("na", i, "jaar zijn er", nieuw, "bomen.")
+    if nieuw == n:
+        break
+    else:
+        n = nieuw
 
 print("Er zullen uiteindelijk", n, "bomen in het bos zijn.")
