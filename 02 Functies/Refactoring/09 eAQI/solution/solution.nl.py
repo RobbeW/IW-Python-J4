0626e0d2d2d1f7b97a4cc263c68835ffe0029447
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2025-01-19 18:54:35 +0100

    Fix typo in solution

--- 02 Functies/Refactoring/09 eAQI/solution/solution.nl.py
+++ 02 Functies/Refactoring/09 eAQI/solution/solution.nl.py
@@ -1,14 +1,14 @@
 def aqi(ozon, nitro):
     eAQI = 0
-    if ozon >= 380 or stikstofdioxide > 340:
+    if ozon >= 380 or nitro > 340:
         eAQI = 6
-    elif ozon >= 240 and ozon < 380 or stikstofdioxide >= 230 and stikstofdioxide < 340:
+    elif ozon >= 240 and ozon < 380 or nitro >= 230 and nitro < 340:
         eAQI = 5
-    elif ozon >= 130 and ozon < 240 or stikstofdioxide >= 120 and stikstofdioxide < 230:
+    elif ozon >= 130 and ozon < 240 or nitro >= 120 and nitro < 230:
         eAQI = 4
-    elif ozon >= 100 and ozon < 130 or stikstofdioxide >= 90 and stikstofdioxide < 120:
+    elif ozon >= 100 and ozon < 130 or nitro >= 90 and nitro < 120:
         eAQI = 3
-    elif ozon >= 50 and ozon < 100 or stikstofdioxide >= 40 and stikstofdioxide < 90:
+    elif ozon >= 50 and ozon < 100 or nitro >= 40 and nitro < 90:
         eAQI = 2
     else:
         eAQI = 1
