3d7949d561bb5cfc82264617f40f1a96513f5618
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-02-24 23:17:02 +0100

    solution fix

--- 02 Functies/21 Vermoeden van Goldbach/solution/solution.nl.py
+++ 02 Functies/21 Vermoeden van Goldbach/solution/solution.nl.py
@@ -12,6 +12,6 @@ if getal % 2 != 0 or getal <= 2:
     print("Het vermoeden is hier niet van toepassing.")
 else:
     for a in range(2, getal // 2 + 1):
-        for b in range(getal // 2, getal):
-            if a + b == getal and is_priem(a) and is_priem(b):
-                print(a, "+", b, "=", getal)
+        b = getal - a
+        if is_priem(a) and is_priem(b):
+            print(a, "+", b, "=", getal)
