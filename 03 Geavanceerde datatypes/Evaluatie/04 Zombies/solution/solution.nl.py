29406cc02dbd154c8ce32f3d08ad828ef7894363
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-05-19 21:02:24 +0200

    Fixing solution

--- 03 Geavanceerde datatypes/Evaluatie/04 Zombies/solution/solution.nl.py
+++ 03 Geavanceerde datatypes/Evaluatie/04 Zombies/solution/solution.nl.py
@@ -11,11 +11,22 @@ def aantal_gezond(sectoren):
 def infectie(sectoren):
     n = len(sectoren)
     nieuw = []
+    
+    if sectoren[0] == 0:
+        nieuw.append(sectoren[1])
+    else:
+        nieuw.append(sectoren[0])
+    
     for i in range(1, n - 1):
         if sectoren[i] == 0:
             gemiddeld = (sectoren[i-1]+sectoren[i+1]) / 2
             nieuw.append(math.floor(gemiddeld))
         else:
             nieuw.append(sectoren[i])
+    
+    if sectoren[n-1] == 0:
+        nieuw.append(sectoren[n-2])
+    else:
+        nieuw.append(sectoren[n-1])
     
     return nieuw
