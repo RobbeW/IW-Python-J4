df8f2d4d5fd74db06089c2fad6485b9e95db5795
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2025-05-26 20:05:58 +0200

    Fixed puzzle 6 solution

--- 03 Geavanceerde datatypes/Quizvragen/Puzzel 6/solution/solution.nl.py
+++ 03 Geavanceerde datatypes/Quizvragen/Puzzel 6/solution/solution.nl.py
@@ -1,6 +1,6 @@
 def even_strings(lijst):
     result = []
-    for i in range(lijst):
-        if len(lijst[i] % 2 == 0):
+    for i in range(len(lijst)):
+        if len(lijst[i]) % 2 == 0:
             result.append(lijst[i])
     return result
