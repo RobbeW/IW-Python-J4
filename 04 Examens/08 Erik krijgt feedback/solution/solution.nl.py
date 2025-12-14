1bc01d30d20ea48d193b29becf2d1af0e3622af3
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2025-12-14 17:23:16 +0100

    Examens 4e - Oefening 8 - Fixed solution

--- 04 Examens/08 Erik krijgt feedback/solution/solution.nl.py
+++ 04 Examens/08 Erik krijgt feedback/solution/solution.nl.py
@@ -2,10 +2,10 @@ leerling = input("Wat is de naam van de leerling?")
 scoreOp20 = int(input("Wat is de score van de leerling? (op 20)"))
 
 if scoreOp20 < 10:
-    print("Je behaalt een onvoldoende voor deze toets", leerling + "je krijgt van mij een remediëring.")
+    print("Je behaalt een onvoldoende voor deze toets", leerling + ", je krijgt van mij een remediëring.")
 elif scoreOp20 <= 13:
     print("Je behaalt een voldoende", leerling + ", maar zal je toch moeten herpakken. Herhaal deze leerstof en maak meer oefeningen voor het examen!")
 elif scoreOp20 < 17:
     print("Beste", leerling + ", dit is een goed resultaat. Doe zo voort!")
 else:
-    print(" Wat een subliem resultaat! Met een", score, "bewijs je talent te hebben voor dit vak!")
+    print(" Wat een subliem resultaat! Met een", scoreOp20, "bewijs je talent te hebben voor dit vak!")
